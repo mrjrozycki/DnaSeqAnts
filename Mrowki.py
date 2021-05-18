a7db36054c6ff0e1700044320d0cbc8419f34a04
Jakub Różycki <kkuubb92@gmail.com>
2021-05-19 01:38:03 +0200

    poprawione mrowki

--- Mrowki.py
+++ Mrowki.py
@@ -2,22 +2,23 @@ import random as rn
 import numpy as np
 from numpy.random import choice as np_choice
 
+
 class AntColony(object):
 
     def __init__(self, odleglosci, ileMrowek, ileNajlepszychMrowek, iteracje, rozkladFeromonu, alpha=1, beta=1):
         """
         Args:
-            odleglosci (2D numpy.array)
-            ileMrowek (int)
-            ileNajlepszychMrowek (int)
-            n_iteration (int)
-            rozkladFeromonu (float)
-            alpha (int or float)
-            beta (int or float)
+            odleglosci (2D numpy.array) - macierz odlegosci
+            ileMrowek (int) - ile mrowek ma ruszac w tym samym momencie
+            ileNajlepszychMrowek (int) - ile najlepszych mrowek ma zostawiac feromon
+            iteracje (int) - ile ma zrobic iteracji
+            rozkladFeromonu (float) - jak szybko rozklada sie feromon (feromon jest mnozony przez ta wartosc wiec np 0.95 to malutki rozklad a 0.5 to ogromyn)
+            alpha (int or float) - waznosc feromonu (def 1)
+            beta (int or float) - waznosc odleglosci (def 1), im wieksza tym bardziej przypomina zachlanny
         Przyklad:
             ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)
         """
-        self.odleglosci  = odleglosci
+        self.odleglosci = odleglosci
         self.feromon = np.ones(self.odleglosci.shape) / len(odleglosci)
         self.ileWierzcholkow = range(len(odleglosci))
         self.ileMrowek = ileMrowek
@@ -31,53 +32,56 @@ class AntColony(object):
         najkrotszaTrasa = None
         ogolnieNajkrotszaTrasa = ("placeholder", np.inf)
         for i in range(self.iteracje):
-            all_paths = self.gen_all_paths()
-            self.spread_pheronome(all_paths, self.ileNajlepszychMrowek, shortest_path=najkrotszaTrasa)
-            najkrotszaTrasa = min(all_paths, key=lambda x: x[1])
-            #print (najkrotszaTrasa)
-            print("Obliczono juz: {:.2f}% algorytmu".format((i+1) * 100 / self.iteracje))
+            wszystkieSciezki = self.wygenerujWszystkieSciezki()
+            self.wypuscFeromon(
+                wszystkieSciezki, self.ileNajlepszychMrowek, shortest_path=najkrotszaTrasa)
+            najkrotszaTrasa = min(wszystkieSciezki, key=lambda x: x[1])
+            # print (najkrotszaTrasa)
+            print("Obliczono juz: {:.2f}% algorytmu".format(
+                (i + 1) * 100 / self.iteracje))
             if najkrotszaTrasa[1] < ogolnieNajkrotszaTrasa[1]:
                 ogolnieNajkrotszaTrasa = najkrotszaTrasa
-            self.feromon * self.rozkladFeromonu
+            # self.feromon * self.rozkladFeromonu
         return ogolnieNajkrotszaTrasa
 
-    def spread_pheronome(self, all_paths, n_best, shortest_path):
-        sorted_paths = sorted(all_paths, key=lambda x: x[1])
-        for path, dist in sorted_paths[:n_best]:
+    def wypuscFeromon(self, wszystkieSciezki, ileNajlepszychMrowek, shortest_path):
+        posortowanaSciezka = sorted(wszystkieSciezki, key=lambda x: x[1])
+        for path, dist in posortowanaSciezka[:ileNajlepszychMrowek]:
             for move in path:
                 self.feromon[move] += 1.0 / self.odleglosci[move]
 
-    def gen_path_dist(self, path):
-        total_dist = 0
+    def podajOdlegloscSciezki(self, path):
+        pelenDystans = 0
         for ele in path:
-            total_dist += self.odleglosci[ele]
-        return total_dist
+            pelenDystans += self.odleglosci[ele]
+        return pelenDystans
 
-    def gen_all_paths(self):
-        all_paths = []
+    def wygenerujWszystkieSciezki(self):
+        wszystkieSciezki = []
         for i in range(self.ileMrowek):
-            path = self.gen_path(0)
-            all_paths.append((path, self.gen_path_dist(path)))
-        return all_paths
+            path = self.wygenerujSciezke(0)
+            wszystkieSciezki.append((path, self.podajOdlegloscSciezki(path)))
+        return wszystkieSciezki
 
-    def gen_path(self, start):
+    def wygenerujSciezke(self, start):
         path = []
-        visited = set()
-        visited.add(start)
+        odwiedzone = set()
+        odwiedzone.add(start)
         prev = start
         for i in range(len(self.odleglosci) - 1):
-            move = self.pick_move(self.feromon[prev], self.odleglosci[prev], visited)
+            move = self.coDalej(
+                self.feromon[prev], self.odleglosci[prev], odwiedzone)
             path.append((prev, move))
             prev = move
-            visited.add(move)
-        path.append((prev, start)) # going back to where we started
+            odwiedzone.add(move)
+        path.append((prev, start))
         return path
 
-    def pick_move(self, pheromone, dist, visited):
-        pheromone = np.copy(pheromone)
-        pheromone[list(visited)] = 0
+    def coDalej(self, feromon, dist, odwiedzone):
+        feromon = np.copy(feromon)
+        feromon[list(odwiedzone)] = 0
 
-        row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)
+        row = feromon ** self.alpha * ((1.0 / dist) ** self.beta)
 
         norm_row = row / row.sum()
         move = np_choice(self.ileWierzcholkow, 1, p=norm_row)[0]
--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import numpy as np
 from Mrowki import AntColony
+
+kosztyDodatkowe = []
+
 def odczytajDane():
     f = open("dane.txt", "r")
     ciagi = []
     for x in f:
         ciagi.append(x.rstrip())
     f.close()
-    #print(ciagi)
+    # print(ciagi)
     return ciagi
 
-def policzOdeleglosc(a,b):
+
+def policzOdeleglosc(a, b):
     if a == b:
         return np.inf
     for i in range(len(a)):
-        #print(a[i:len(a)],"\n",b[0:len(b)-i])
-        if a[i:len(a)] == b[0:len(b)-i]:
-            #print(i)
+        # print(a[i:len(a)],"\n",b[0:len(b)-i])
+        if a[i:len(a)] == b[0:len(b) - i]:
+            # print(i)
             break
     if i == 9:
-        return i+10
+        return i + 10
     else:
         return i
+
 
 def macierzOdlegosci(ciagi):
     dlugosc = len(ciagi)
@@ -34,9 +39,9 @@ def macierzOdlegosci(ciagi):
 
 ciagi = odczytajDane()
 gotowa = macierzOdlegosci(ciagi)
-ant_colony = AntColony(gotowa, 200, 50, 30, 0.7, alpha=1, beta=1)
+ant_colony = AntColony(gotowa, 200, 5, 30, 0.7, alpha=1, beta=1)
 najkrotsza = ant_colony.run()
-#print ("Najkrotsza trasa to: {}".format(najkrotsza))
+# print ("Najkrotsza trasa to: {}".format(najkrotsza))
 pierwsze = True
 for i in najkrotsza[0]:
     pierwsza = i[0]
@@ -46,10 +51,7 @@ for i in najkrotsza[0]:
         pierwsze = False
     else:
         print(ciagi[druga], end=" ")
-print("\nKoszt takiej sekwencji to:", najkrotsza[1])
-    #print(i[0], i[1])
-#print(najkrotsza[0][1])
-
-
-
-
+print("\nKoszt takiej sekwencji to:", najkrotsza[1] - len(ciagi))
+print(kosztyDodatkowe)
+# print(i[0], i[1])
+# print(najkrotsza[0][1])
